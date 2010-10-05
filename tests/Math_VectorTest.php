ff6c9bdadcf674723215e74121570c2abfddfcdb
clockwerx <clockwerx@php.net>
2010-10-05 00:23:00 +0000

    Updated coverage (if somewhat fragile)

--- tests/Math_VectorTest.php
+++ tests/Math_VectorTest.php
@@ -1,9 +1,11 @@
 <?php
-require_once "Math/Vector/Vector.php";
-require_once "Math/Vector/Vector2.php";
-require_once "Math/Vector/Vector3.php";
+require_once "Math/Vector.php";
+require_once "Math/Vector2.php";
+require_once "Math/Vector3.php";
 
-class Math_VectorTest extends PHPUnit_TestCase {
+require_once 'PHPUnit/Framework.php';
+
+class Math_VectorTest extends PHPUnit_Framework_TestCase {
 	var $v;
 	var $w;
 	var $z;
@@ -12,10 +14,6 @@ class Math_VectorTest extends PHPUnit_TestCase {
 	var $a;
 	var $t;
 	var $bad;
-
-	function VectorCreationTest($name) {
-		$this->PHPUnit_TestCase($name);
-	}
 
 	function setup() {
 		$this->a = array(2,3,4);
@@ -72,4 +70,64 @@ class Math_VectorTest extends PHPUnit_TestCase {
 		$this->v->reverse();
 		$this->assertTrue($this->v->toString() == "Vector: < -2, -3, -4 >");
 	}
+
+    function testTuple() {
+        ob_start();
+?>
+Creating tuple
+Math_Tuple Object
+(
+    [data] => Array
+        (
+            [0] => 2
+            [1] => 3
+            [2] => 4
+        )
+
+)
+Tuple size: 3
+Adding element value 7
+Tuple size: 4
+Tuple toString: { 2, 3, 4, 7 }
+Setting element 1 to 11
+Tuple toString: { 2, 11, 4, 7 }
+Maximum element value: 11
+Minimum element value: 2
+Index of maximum value: 1
+Index of minimum value: 0
+Remove element 2
+Tuple toString: { 2, 11, 7 }
+Get element 2: 7
+Get index of element value 11: 1
+<?php
+        $expected = ob_get_clean();
+
+        ob_start();
+
+        echo "Creating tuple\n";
+        $t = new Math_Tuple(array(2,3,4));
+        print_r($t);
+        echo "Tuple size: ".$t->getSize()."\n";
+        echo "Adding element value 7\n";
+        $t->addElement(7);
+        echo "Tuple size: ".$t->getSize()."\n";
+        echo "Tuple toString: ".$t->toString()."\n";
+        echo "Setting element 1 to 11\n";
+        $t->setElement(1, 11);
+        echo "Tuple toString: ".$t->toString()."\n";
+        echo "Maximum element value: ".$t->getMax()."\n";
+        echo "Minimum element value: ".$t->getMin()."\n";
+        echo "Index of maximum value: ".$t->getMaxIndex()."\n";
+        echo "Index of minimum value: ".$t->getMinIndex()."\n";
+        echo "Remove element 2\n";
+        $t->delElement(2);
+        echo "Tuple toString: ".$t->toString()."\n";
+        echo "Get element 2: ".$t->getElement(2)."\n";
+        echo "Get index of element value 11: ".$t->getValueIndex(11)."\n";
+
+        $result = ob_get_clean();
+
+        $this->assertSame($expected, $result);
+
+    }
 }
